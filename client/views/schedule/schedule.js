ad60c63aeb606cb53c163e2f10e11c555f7fb124
John Sabilla <johnesabilla@gmail.com>
2015-03-08 15:33:36 -0700

    committing random stuff

--- client/views/schedule/schedule.js
+++ client/views/schedule/schedule.js
@@ -3,15 +3,30 @@ Template['schedule'].helpers({
 	schedules : function(){ 
     var sched = schedules.find({});
 		return sched;
-	}
+	},
 });
 
+
 Template.schedule.events({
+
+
   'click a' : function(event,template){
    // $('.list-group-item').addClass('list-group-item-success');
-   console.log(template.data._id); //this gets the id
-   $('#'+event.target.id).addClass('list-group-item-success');
+   console.log(schedules.findOne({ _id: new Meteor.Collection.ObjectID(template.data._id._str)})); //this gets the id
+   console.log(event.target.id);
+   console.log(this);
 
+   var test = schedules.findOne({ _id: new Meteor.Collection.ObjectID(template.data._id._str)});
+
+   if(this.completed == 0){
+   		console.log("this", test);
+   		console.log("hello fuckface ", this.count);
+
+   		//updateSchedule(template.data.id, this.count );
+   		schedules.update({ '_id' : template.data._id }, { 'schedule': { 'completed' : 1  } });
+   		//db.schedules.update({ "_id" : ObjectId("54fa2a3ec957445b162b58e7"), "schedule.count" : 14}, {"$set" : {"schedule.$.completed" : 1  }})
+   }
+   $('#'+event.target.id).addClass('list-group-item-success');
   }
 });
 
@@ -19,8 +34,6 @@ Template.schedules.rendered = function(){
 
 }
 Template.schedule.rendered = function(){
-
-
 }
 
 
--- models/schedules.js
+++ models/schedules.js
@@ -1,1 +1,33 @@
 schedules = new Mongo.Collection('schedules');
+
+
+if (Meteor.isClient) {
+
+  schedules.allow({
+    insert : function () {
+      return true;
+    },
+    update : function () {
+      return true;
+    },
+    remove : function () {
+      return true;
+    }
+  });
+}
+
+
+
+if (Meteor.isServer) {
+  schedules.allow({
+    insert : function () {
+      return true;
+    },
+    update : function () {
+      return true;
+    },
+    remove : function () {
+      return true;
+    }
+  });
+}
